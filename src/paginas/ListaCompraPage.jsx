4313abddc58c8059005ef020571435b81171f8d2
Sergio Alfonso Deltell <sergioalfonso.alu@iespacomolla.es>
2024-01-18 19:01:34 +0100

    Ajustes de css, mas responsive

--- src/paginas/ListaCompraPage.jsx
+++ src/paginas/ListaCompraPage.jsx
@@ -1,15 +1,11 @@
 import {Fragment} from 'react'
-import Listado from '../componentes/Principal/Listado/Listado.jsx'   
-import DetailsForms from '../componentes/Principal/DetailsForms/DetailsForms.jsx'
+import Listado from '../componentes/Principal/Listado/Listado.jsx'
 
 const ListaCompraPage = () => {
   return (
     <Fragment>
         <h2 id='principal-h2'>Listado</h2>
-        <section id='info'>
-          <Listado/>
-          <DetailsForms/>
-        </section>
+        <Listado/>
     </Fragment>
   )
 }
