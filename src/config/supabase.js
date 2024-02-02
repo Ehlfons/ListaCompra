e84acb6326ac3d2225087c4c3446d8460754c2a8
Sergio Alfonso <sergioalfonso.alu@iespacomolla.es>
2024-02-02 19:00:54 +0100

    proyecto nuevo para usuarios

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseConexion = createClient(
-  "https://kbklonufmcjitkdzrncz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtia2xvbnVmbWNqaXRrZHpybmN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4OTc5MzksImV4cCI6MjAyMDQ3MzkzOX0.roaINVE5mXS_ZbXjipRoZ4DgBFeLBQ3rLIepQXUbMa4"
+  "https://xstxkftktbmcowymiexb.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzdHhrZnRrdGJtY293eW1pZXhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4OTU0MTIsImV4cCI6MjAyMjQ3MTQxMn0.DDwuEDqxgbeeYJi6V13J3Ahw2GRTP864b7NTAxD0dgg"
 );
 
 export { supabaseConexion };
---
+++ src/modales/CrearProductoModal.jsx
@@ -0,0 +1,69 @@
+import { Fragment } from "react";
+import useProductos from "../hooks/useProductos.jsx";
+import "./Modales.css";
+
+const CrearListasModal = ({ mostrar, manejarCerrado }) => {
+  const {  } = useProductos();
+  
+  const manejarClick = (e) => {
+    const { esValido, errores } = validarFormulario(lista); // Validar el formulario.
+
+    if (esValido) {
+      insertLista();
+      manejarCerrado();
+      actualizarIdListaActual("");
+    } else {
+      actualizarErroresLista(errores); // Actualizar el estado de los errores.
+    }
+  };
+
+  return (
+    <Fragment>
+      {mostrar && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <div className="modal-header">
+              <h2>Crear Lista</h2>
+              <span className="cerrar-modal" onClick={manejarCerrado}>
+                &times;
+              </span>
+            </div>
+            <div className="modal-body">
+              <input
+                type="text"
+                name="nombre"
+                placeholder="Nombre de la lista"
+                id="input-nombre-lista"
+                value={lista.lista_nombre}
+                onChange={(e) => {
+                  createLista(e.target.value);
+                  // Limpiar el error al cambiar el contenido del campo.
+                  actualizarErroresLista((prevErrores) => ({
+                    ...prevErrores,
+                    lista_nombre: null,
+                  }));
+                }}
+              />
+              {erroresLista.lista_nombre ? <small>{erroresLista.lista_nombre}</small> : null}
+            </div>
+            <div className="modal-footer">
+              <button className="btn btn-cancelar" onClick={manejarCerrado}>
+                Cancelar
+              </button>
+              <button
+                className="btn btn-confirmar"
+                onClick={(e) => {
+                  manejarClick(e);
+                }}
+              >
+                Crear lista
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </Fragment>
+  );
+};
+
+export default CrearListasModal;
